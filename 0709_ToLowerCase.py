9a3e8f69bf1abbee7dfe9d8f59e259b2d3c54d9e
naivelamb <davidcao1991@gmail.com>
2019-03-08 17:59:47 -0500

    Update 0709_ToLowerCase.py

--- 0709_ToLowerCase.py
+++ 0709_ToLowerCase.py
@@ -3,7 +3,10 @@
 https://leetcode.com/problems/to-lower-case/
 
 Use order, chr transform
-Time complexity: O(n)
+Time complexity: 
+If using word += chr, it might be O(n^2) depends on the CPython version. 
+It is suggested on wiki.python.org that best algorithms for string concatenation
+is ''.join(seq), which is guarenteed to be O(n) 
 """
 class Solution:
     def toLowerCase(self, str: str) -> str:
@@ -15,7 +18,16 @@ class Solution:
             else:
                 ans += ch
         return ans
-    
+
+    def toLowerCase_join(self, str: str) -> str:
+        ans = []
+        delta = ord('a') - ord('A')
+        for ch in str:
+            if ord('A') <= ord(ch) <= ord('Z'):#capital
+                ans.append(chr(ord(ch) + delta))
+            else:
+                ans.append(ch)
+        return ''.join(ans)
 s = Solution()
 a = 'Hello'
 print(s.toLowerCase(a))
