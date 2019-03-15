af6e180f490f3fc2fa5975c0041ed31683df1593
naivelamb <davidcao1991@gmail.com>
2019-03-15 15:42:37 -0400

    Update 0416_PartitionEqualSubsetSum.py

--- 0416_PartitionEqualSubsetSum.py
+++ 0416_PartitionEqualSubsetSum.py
@@ -2,10 +2,15 @@
 """
 https://leetcode.com/problems/partition-equal-subset-sum/
 
+#1 DFS
 We try to build subset that gives the target (sum(nums) // 2). => DFS 
 Sort to avoid duplication. 
 
-Time complexity:
+#2 DP
+dp[i][j] => can nums[:i+1] for subset whose sum is j?
+dp[i][j] = dp[i-1][j-nums[i-1]] if j >= nums[i-1]
+
+Time complexity: O(mn), m = sum(target) // 2
 """
 class Solution:
     def canPartition(self, nums):
@@ -28,3 +33,19 @@ class Solution:
             return False
         
         return dfs(0, target)
+    
+    def canPartition_dp(self, nums):
+        total = sum(nums)
+        n = len(nums)
+        if total % 2: return False
+        target = total // 2
+        dp = [[False] * (target + 1) for _ in range(n + 1)]
+        # dp[i][j]nums[:i+1] can sum to j
+        dp[0][0] = True
+        for i in range(1, n + 1):
+            for j in range(target + 1):
+                dp[i][j] = dp[i-1][j]
+                if j - nums[i - 1] >= 0 and dp[i-1][j - nums[i-1]]:
+                    dp[i][j] = True
+        return dp[n][target] 
+                        
