4fec3508401e0aaf0068c4dcdca7047b24e0710b
xuan.cao@walmartlabs.com <xuan.cao@walmartlabs.com>
2020-05-27 10:10:36 -0700

    add 0886, 0986

--- 0986_IntervalListIntersections.py
+++ 0986_IntervalListIntersections.py
@@ -2,8 +2,8 @@
 """
 https://leetcode.com/problems/interval-list-intersections/
 
-Two pointers. 
-Do intersection between the interval. 
+Two pointers.
+Do intersection between the interval.
 Keep the interval with larger end, and move the other forward
 Time complexity: O(m + n), m = len(A), n = len(B)
 """
@@ -16,7 +16,7 @@ class Interval:
 class Solution:
     def intervalIntersection(self, A: 'List[Interval]', B: 'List[Interval]') -> 'List[Interval]':
         def merge(a, b):
-            # get intersection of two intervals. 
+            # get intersection of two intervals.
             l = max(a.start, b.start)
             r = min(a.end, b.end)
             res = []
@@ -32,6 +32,29 @@ class Solution:
             if res:
                 ans.append(res)
             if a.end < b.end:
+                i += 1
+            else:
+                j += 1
+        return ans
+
+    def intervalIntersection_new(self, A, B):
+        def merge(a, b):
+            l = max(a[0], b[0])
+            r = min(a[1], b[1])
+            res = []
+            if l <= r:
+                res = [l, r]
+            return res
+
+        ans = []
+        i, j = 0, 0
+        m, n = len(A), len(B)
+        while i < m and j < n:
+            a, b = A[i], B[j]
+            res = merge(a, b)
+            if res:
+                ans.append(res)
+            if a[1] < b[1]:
                 i += 1
             else:
                 j += 1
