6b7efc45efa1707b37d3482338c42214dfb60dbf
xuan@linux <davidcao1991@gmail.com>
2020-06-06 11:06:02 -0700

    add 0406

--- 0406_QueueReconstructionByHeight.py
+++ 0406_QueueReconstructionByHeight.py
@@ -4,17 +4,17 @@ https://leetcode.com/problems/queue-reconstruction-by-height/
 
 #1 Greedy
 We know where to put the shortest guy, ans[k].
-Then for the next shortest guy, we need to put him at the k-th empty slot. 
+Then for the next shortest guy, we need to put him at the k-th empty slot.
 Time complexity: O(nlogn + n^2)
 
 #2 Insert order
-We can start the procedure from the tallest people. Take 
-[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]] 
+We can start the procedure from the tallest people. Take
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
 as an example, for people of height 7, we know that,
 [[7, 0], [7, 1]] 
-Then [6, 1] is inserted at location 1. 
+Then [6, 1] is inserted at location 1.
 [[7, 0], [6, 1], [7, 1]]
-And we keep this process going until all people is placed. 
+And we keep this process going until all people is placed.
 Time Complexity: O(nlogn + n^2)
 """
 class Solution:
@@ -39,4 +39,4 @@ class Solution:
         res = []
         for p in people:
             res.insert(p[1], p)
-        return res   
+        return res
