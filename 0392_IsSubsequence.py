ebded647acc5ea0a2a13c80a55b59663e2cac7ec
xuan@linux <davidcao1991@gmail.com>
2020-06-09 10:21:02 -0700

    add 0392

---
+++ 0392_IsSubsequence.py
@@ -0,0 +1,15 @@
+"""
+https://leetcode.com/problems/is-subsequence/
+"""
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        p1, p2 = 0, 0
+        match = 0
+        while p1 < len(s) and p2 < len(t):
+            if s[p1] == t[p2]:
+                p1 += 1
+                p2 += 1
+                match += 1
+            else:
+                p2 += 1
+        return match == len(s)
