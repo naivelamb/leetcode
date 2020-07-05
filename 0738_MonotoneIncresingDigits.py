63fbae3b8cc5a6ee77f83e899478ccb785eab449
xuan@linux <davidcao1991@gmail.com>
2020-07-05 10:28:02 -0700

    add 0461, 0738

---
+++ 0738_MonotoneIncresingDigits.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/monotone-increasing-digits/
+Build digit by digit.
+
+"""
+class Solution:
+    def monotoneIncreasingDigits(self, N: int) -> int:
+        digits = []
+        A = list(map(int, str(N)))
+        for i in range(len(A)):
+            for d in range(1, 10):
+                if digits + [d] * (len(A) - i) > A:
+                    digits.append(d-1)
+                    break
+            else:
+                digits.append(9)
+        return int("".join(map(str, digits))) 
