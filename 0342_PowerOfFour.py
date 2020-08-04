9f9670ba5adbcff6d18ab58620c3741e6d6e5995
xuan@linux <davidcao1991@gmail.com>
2020-08-04 09:42:44 -0700

    add 0341, 0342

---
+++ 0342_PowerOfFour.py
@@ -0,0 +1,6 @@
+"""
+https://leetcode.com/problems/power-of-four/
+"""
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        return num != 0 and num&(num-1) == 0 and len('{0:b}'.format(num)) % 2 == 1
