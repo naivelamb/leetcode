bad1640bf5a7edd557f74c7698d02792980e021f
xuan@linux <davidcao1991@gmail.com>
2020-08-09 10:43:15 -0700

    add time complexity for 0994

--- 0994_RottingOranges.py
+++ 0994_RottingOranges.py
@@ -3,6 +3,8 @@
 https://leetcode.com/problems/rotting-oranges/
 
 BFS starting from multiple points simultaneously.
+
+Time Complexity: O(mn)
 """
 
 import collections
@@ -11,18 +13,18 @@ class Solution:
         fresh = set()
         m, n = len(grid), len(grid[0])
         queue = collections.deque()
-        
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1: #fresh
                     fresh.add((i, j))
                 if grid[i][j] == 2: #rotten
                     queue.append((i, j, 0))
-        
+
         res = 0
         if not fresh:
             return res
-        
+
         while queue:
             i, j, t = queue.popleft()
             res = max(res, t)
@@ -30,9 +32,7 @@ class Solution:
                 if 0 <= x < m and 0 <= y < n and (x, y) in fresh:
                     queue.append((x, y, t + 1))
                     fresh.remove((x, y))
-        
+
         if fresh:
             return -1
         return res
-                    
-        
