c2ad09dde21e4837ac9f4f23841132a578129cd8
xuan@linux <davidcao1991@gmail.com>
2020-08-28 09:34:50 -0700

    add 0470

---
+++ 0470_ImplementRand10UsingRand7.py
@@ -0,0 +1,16 @@
+"""
+https://leetcode.com/problems/implement-rand10-using-rand7/
+"""
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution:
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        ans = 0
+        for i in range(10):
+            ans += rand7()
+        return (an % 10) + 1
