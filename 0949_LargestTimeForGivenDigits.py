b9a22a0101786599076c8de5dd9d21cc0b9c4c64
xuan@linux <davidcao1991@gmail.com>
2020-09-01 13:01:44 -0700

    add 0157, 0949

---
+++ 0949_LargestTimeForGivenDigits.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/largest-time-for-given-digits/
+"""
+class Solution:
+    def largestTimeFromDigits(self, A: List[int]) -> str:
+        ans = -1
+        for h1, h2, m1, m2 in itertools.permutations(A):
+            hours = h1*10 + h2
+            mins = m1*10 + m2
+            time = hours*60 + mins
+            if 0 <= hours < 24 and 0 <= mins < 60 and time > ans:
+                ans = time
+        if ans >= 0:
+            hours = ans // 60
+            mins = ans % 60
+            return f"{hours}:{mins}"
+        else:
+            return ""
