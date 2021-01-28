a8444ce83ef27dc46b7634e64034150154a4b06e
xuan@linux <davidcao1991@gmail.com>
2021-01-28 10:09:24 -0800

    modify 1663

--- 1663_SmallestStringWithAGivenNumericValue.py
+++ 1663_SmallestStringWithAGivenNumericValue.py
@@ -3,6 +3,18 @@ https://leetcode.com/problems/smallest-string-with-a-given-numeric-value/
 
 Greedy. Fill head and tail with 'a' and 'z' as many as possible. 
 
+Let's say we have na 'a', nz 'z', and nm non-'az' characters. 
+
+na + nz + nm = n
+na + nz*26 + nm*mid = k
+
+We know nz = (k - n)/25 - nm * (mid - 1)/25
+(mid - 1) < 25, nm <= 1, hence nz = (k - n)//25
+If (k - n) % 25 == 0, nm = 0 else nm = 1
+Then we know na = n - nm - nz
+
+Finally we can assemble the string.
+
 Time complexity: O(n) ==> most of time is at building string.
 """
 class Solution:
