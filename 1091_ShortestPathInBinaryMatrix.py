820e543dd09a138d047fe966bb416c62716eea3d
xuan@linux <davidcao1991@gmail.com>
2021-02-13 10:33:18 -0800

    update 1091

--- 1091_ShortestPathInBinaryMatrix.py
+++ 1091_ShortestPathInBinaryMatrix.py
@@ -1,18 +1,18 @@
 """
 https://leetcode.com/problems/shortest-path-in-binary-matrix/
 
-BFS + Heap 
-Time complexity: O(MNlogMN)
+BFS
+Time complexity: O(MN)
 """
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         visited = set()
-        heap = [(1, 0, 0)]
+        queue = collections.deque([(1, 0, 0)])
         n = len(grid)
         if grid[0][0] == 1:
             return -1
-        while heap:
-            step, i, j = heapq.heappop(heap)
+        while queue:
+            step, i, j = queue.popleft()
             if (i, j) == (n-1, n-1):
                 return step
             if (i, j) in visited:
@@ -22,5 +22,5 @@ class Solution:
             for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]:
                 new_i, new_j = i + di, j + dj
                 if 0 <= new_i < n and 0 <= new_j < n and grid[new_i][new_j] == 0 and (new_i, new_j) not in visited:
-                    heapq.heappush(heap, (step + 1, new_i, new_j))
+                    queue.append((step + 1, new_i, new_j))
         return -1
